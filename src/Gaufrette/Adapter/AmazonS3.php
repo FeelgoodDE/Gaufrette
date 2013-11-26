958700fd08485649918135890ceed5223c29a373
Oliver Buschjost <oliver.buschjost@icans-gmbh.com>
2013-11-26 13:32:08 +0100

    Also increase max-keys if prefix filtering is not active

--- src/Gaufrette/Adapter/AmazonS3.php
+++ src/Gaufrette/Adapter/AmazonS3.php
@@ -208,8 +208,9 @@ class AmazonS3 extends Base
 		$options = array();
 	    if (null !== $prefix) {
 	        $options['prefix'] = $prefix;
-	        $options['max-keys'] = "100000";
 	    }
+        $options['max-keys'] = "100000";
+        
         $response = $this->service->list_objects($this->bucket, $options);
         if (!$response->isOK()) {
             throw new \RuntimeException('Could not get the keys.');
